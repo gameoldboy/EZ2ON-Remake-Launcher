e10fd6caa5a1867c652c99da9d00984595efc49b
大fa师 <chfreezer00@gmail.com>
2019-06-11 13:10:23 +0800

    更新歌曲包地址

--- src/index.js
+++ src/index.js
@@ -81,6 +81,8 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+    // 不管了先退出
+    app.quit();
   });
 
   mainWindow.once('ready-to-show', () => {
@@ -110,9 +112,9 @@ if (!gotTheLock) {
   app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
+    // if (process.platform !== 'darwin') {
+    app.quit();
+    // }
   });
 
   app.on('activate', () => {
